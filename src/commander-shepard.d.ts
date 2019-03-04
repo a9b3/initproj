02135da33e22c28f1e9b5470463d8b5376ee53db
Sam Lau <esayemm@gmail.com>
2019-03-04 02:03:58 -0800

    add commander-shepard package

---
+++ src/commander-shepard.d.ts
@@ -0,0 +1,4 @@
+declare module "commander-shepard" {
+  export const Commander: any
+  export const Command: any
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(`hi`)
+import { Commander, Command } from 'commander-shepard'
+
+const commander = new Commander({
+  key: 'commander',
+  packageJson: require('../package.json'),
+  handler: () => {
+    console.log(`here`)
+  },
+})
+
+commander.start()
+.catch(console.error)
