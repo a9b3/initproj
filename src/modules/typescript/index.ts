e70d6ae67d8bbf1e4903814265550b88392dfe24
Sam Lau <esayemm@gmail.com>
2019-03-05 01:50:42 -0800

    remove unused

--- src/modules/typescript/index.ts
+++ src/modules/typescript/index.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import { shell, mergeJSON } from 'task-api'
 
 export default function typescript({ commands, flags }: any) {
-  shell(`cd ${commands[1]} && yarn add --dev typescript tsc ts-node tslib`)
+  shell(`cd ${commands[1]} && yarn add --dev typescript tsc`)
 
   mergeJSON({
     dest: path.resolve(process.cwd(), commands[1], 'tsconfig.json'),
